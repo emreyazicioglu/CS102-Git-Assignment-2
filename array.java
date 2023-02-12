4662ee519de109e0d967dbcbcc9c7f077150fd7a
İlke Latifoğlu <ilkeltfgl@gmail.com>
2023-02-12 14:43:28 +0300

    1st task
    A method to create an integer array containing integers from the interval [0,100]

---
+++ array.java
@@ -0,0 +1,19 @@
+/** 
+ * This programme creates an int array with random integers in the range of [0,100]
+ * @author İlke Latifoğlu
+ * Student ID: 22203818
+ */
+
+public class array {
+
+    public static int[] createArray (int arraySize){
+
+        int[] newArray = new int[arraySize];
+        for (int i = 0; i < newArray.length; i++){
+            newArray[i] = (int) (Math.round(Math.random() * 100));
+        }
+        return newArray;
+
+    }
+
+}
